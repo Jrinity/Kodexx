37b7abb962e6b464c3a099be8c6d1c5818864d1c
Jrinity <jeronimoal10@gmail.com>
2017-03-10 18:16:51 -0500

    Se ve súper bonito

---
+++ Ejercicio1/js/script.js
@@ -0,0 +1,8 @@
+/*alert("Holi mundito")
+var x=2+3
+alert(x)*/
+function meLlamaron(){
+    //alert("Esta gente abusa de mi, jalp mi estimado "+document.getElementById("nombre").value)
+    document.getElementById("idNice").innerHTML="WTF!!";
+    return false;
+}
